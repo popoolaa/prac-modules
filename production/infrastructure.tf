8ca567129433fc9e4eafe17a8e0fcbdb2ab0594b
popoolaa <aademolapopoola@gmail.com>
2025-03-20 13:19:47 -0400

    prod_sg

--- production/infrastructure.tf
+++ production/infrastructure.tf
@@ -7,3 +7,11 @@ module "prod_vpc_1" {
   private_cidr_block = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
   azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
   }
+
+module "prod_sg_1" {
+  source = "../modules/sg"
+  vpc_name = module.prod_vpc_1.vpc_name
+  vpc_id = module.prod_vpc_1.vpc_id
+  ingress_value = ["80", "443", "445", "8080", "22", "3389"]
+  environment = module.prod_vpc_1.environment
+}
