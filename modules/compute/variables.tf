f4ab621a7b9a42824020ab8d040e7b8335711dba
popoolaa <aademolapopoola@gmail.com>
2025-03-24 11:45:00 -0400

    added

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,7 +2,7 @@ variable "environment" {}
 variable "amis" {}
 variable "aws_region" {}
 variable "key_name" {}
-variable "public-subnets" {}
-variable "private-subnets" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
 variable "vpc_name" {}
 variable "sg_id" {}
