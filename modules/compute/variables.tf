c8229f8a336ceca81c91abe8aa4c8f26159531ed
popoolaa <aademolapopoola@gmail.com>
2025-03-24 10:34:29 -0400

    remove env

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -4,6 +4,5 @@ variable "aws_region" {}
 variable "key_name" {}
 variable "public_subnets" {}
 variable "private_subnets" {}
-variable "sg_id" {}
-variable "environment" {}
 variable "vpc_name" {}
+variable "sg_id" {}
