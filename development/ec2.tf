607d527a70d8adf8f3d6922ceaedd1d40fe6c2b5
popoolaa <aademolapopoola@gmail.com>
2025-03-24 10:55:10 -0400

    rmv index

--- development/ec2.tf
+++ development/ec2.tf
@@ -5,7 +5,7 @@ module "dev_ec2_1" {
     us-east-1 = "ami-0263e4deb427da90e"
     us-east-2 = "ami-04f167a56786e4b09"
   }
-  aws_region = var.regions
+  aws_region = var.aws_region
   key_name = "myKey2021"
   public_subnets = module.dev_vpc_1.public-subnet
   private_subnets = module.dev_vpc_1.private-subnet
