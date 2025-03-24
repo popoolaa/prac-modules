f1a7b82af36d5adfdd278891aa16a0df7efda4fb
popoolaa <aademolapopoola@gmail.com>
2025-03-24 12:23:08 -0400

    added

--- modules/compute/public_ec2.tf
+++ modules/compute/public_ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "public-server" {
   ami                         = lookup(var.amis, var.aws_region)
   instance_type               = "t2.micro"
   key_name                    = var.key_name
-  subnet_id                   = element(var.public_subnets, count.index)
+  subnet_id                   = element(var.public-subnets, count.index)
   vpc_security_group_ids      = [var.sg_id]
   associate_public_ip_address = true
   tags = {
